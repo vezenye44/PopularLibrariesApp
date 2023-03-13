2399e384ca501624275dd3ac9216531c54d962a8
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-13 20:16:37 +0300

    Работа с Moxy | Добавили интерфейс для элемента списка пользователей Github - UserItemView.kt и соответственно типизированный интерфейс IUserListPresenter.kt.

---
+++ app/src/main/java/com/example/popularlibrariesapp/ui/users/IUserListPresenter.kt
@@ -0,0 +1,6 @@
+package com.example.popularlibrariesapp.ui.users
+
+import com.example.popularlibrariesapp.ui.interfaces.list.IListPresenter
+
+interface IUserListPresenter: IListPresenter<UserItemView> {
+}
---
+++ app/src/main/java/com/example/popularlibrariesapp/ui/users/UserItemView.kt
@@ -0,0 +1,7 @@
+package com.example.popularlibrariesapp.ui.users
+
+import com.example.popularlibrariesapp.ui.interfaces.list.IItemView
+
+interface UserItemView: IItemView {
+    fun setLogin(text: String)
+}
