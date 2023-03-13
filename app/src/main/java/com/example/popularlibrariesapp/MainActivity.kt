721f19fa24f33242349ea650277ecd72b9f8317d
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-13 16:37:20 +0300

    Простая реализация счетчиков | Вся логика в MainActivity.kt.

--- app/src/main/java/com/example/popularlibrariesapp/MainActivity.kt
+++ app/src/main/java/com/example/popularlibrariesapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.popularlibrariesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.popularlibrariesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counters = mutableListOf(0, 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setCounters()
+
+        binding.mainActivityCounterOneBtn.setOnClickListener {
+            binding.mainActivityCounterOneBtn.text = (++counters[0]).toString()
+        }
+        binding.mainActivityCounterTwoBtn.setOnClickListener {
+            binding.mainActivityCounterTwoBtn.text = (++counters[1]).toString()
+        }
+        binding.mainActivityCounterThreeBtn.setOnClickListener {
+            binding.mainActivityCounterThreeBtn.text = (++counters[2]).toString()
+        }
+    }
+
+    private fun setCounters() {
+        binding.mainActivityCounterOneBtn.text = counters[0].toString()
+        binding.mainActivityCounterTwoBtn.text = counters[1].toString()
+        binding.mainActivityCounterThreeBtn.text = counters[2].toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(EXTRA_COUNTERS, counters.toIntArray())
+    }
+
+    override fun onSaveInstanceState(
+        outState: Bundle, outPersistentState:
+        PersistableBundle
+    ) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putIntArray(EXTRA_COUNTERS, counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val countersArray = savedInstanceState.getIntArray(EXTRA_COUNTERS)
+        countersArray?.toList()?.let {
+            counters.clear()
+            counters.addAll(it)
+        }
+        setCounters()
+    }
+
+    companion object {
+        private const val EXTRA_COUNTERS = "counters"
     }
 }
