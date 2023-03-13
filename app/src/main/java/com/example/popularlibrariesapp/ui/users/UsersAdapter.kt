4a268bed922ff54dd82ef372fe5bfc9c98baf0e5
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-13 20:20:33 +0300

    Работа с Moxy | Добавили UsersAdapter.kt для RecyclerView со списком пользователей.

---
+++ app/src/main/java/com/example/popularlibrariesapp/ui/users/UsersAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.popularlibrariesapp.ui.users
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UsersAdapter(private val presenter: IUserListPresenter) :
+    RecyclerView.Adapter<UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        UserViewHolder(parent).apply {
+            itemView.setOnClickListener {
+                presenter.itemClickListener?.invoke(this)
+            }
+        }
+
+    override fun getItemCount() = presenter.getCount()
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        return presenter.bindView(holder.apply { pos = position })
+    }
+
+}
