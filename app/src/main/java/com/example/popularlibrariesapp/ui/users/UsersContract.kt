34c36e4f2688bac0e52aebbafc60ee5e97a4d57a
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-13 20:23:58 +0300

    Работа с Moxy | Изменили контракт для View

---
+++ app/src/main/java/com/example/popularlibrariesapp/ui/users/UsersContract.kt
@@ -0,0 +1,13 @@
+package com.example.popularlibrariesapp.ui.users
+
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+interface UsersContract {
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    interface View : MvpView {
+        fun init()
+        fun updateList()
+    }
+}
