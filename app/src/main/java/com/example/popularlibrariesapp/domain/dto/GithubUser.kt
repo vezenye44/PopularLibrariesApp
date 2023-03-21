53551f66990406976d5d9b2b61f888d59840bc5d
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-21 16:34:00 +0300

    -\\-

--- app/src/main/java/com/example/popularlibrariesapp/domain/dto/GithubUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.popularlibrariesapp.domain.dto
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class GithubUser(
-    val login: String
-) : Parcelable
