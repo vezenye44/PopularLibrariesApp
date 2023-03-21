54ed51aab64e1228026b0038422b7c97fe983a1f
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-21 16:28:42 +0300

    Изменяем фейковую имплементацию репозитория.

--- app/src/main/java/com/example/popularlibrariesapp/data/repo/FakeGithubUsersRepoImpl.kt
+++ app/src/main/java/com/example/popularlibrariesapp/data/repo/FakeGithubUsersRepoImpl.kt
@@ -1,20 +1,17 @@
 package com.example.popularlibrariesapp.data.repo
 
-import com.example.popularlibrariesapp.domain.dto.GithubUser
+import com.example.popularlibrariesapp.domain.dto.GithubUserEntity
 import com.example.popularlibrariesapp.domain.repo.GithubUsersRepo
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
-class FakeGithubUsersRepoImpl: GithubUsersRepo {
+class FakeGithubUsersRepoImpl : GithubUsersRepo {
 
     private val repositories = listOf(
-        GithubUser("login1"),
-        GithubUser("login2"),
-        GithubUser("login3"),
-        GithubUser("login4"),
-        GithubUser("login5")
+        GithubUserEntity("mojombo", "1", "https://avatars.githubusercontent.com/u/1?v=4"),
+        GithubUserEntity("defunkt", "2", "https://avatars.githubusercontent.com/u/2?v=4"),
+        GithubUserEntity("pjhyett", "3", "https://avatars.githubusercontent.com/u/3?v=4")
     )
 
-    override fun getUsers() : List<GithubUser> = repositories
-    override fun getUsersByRx(): Observable<List<GithubUser>> = Observable.just(repositories)
+    override fun getUsers() : Single<List<GithubUserEntity>> = Single.just(repositories)
 
 }
