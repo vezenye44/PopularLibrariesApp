d952ae6b30c65dc8c39b71e36c0356a82a002e35
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-27 18:20:50 +0300

    + Инстансы базы данных и проверки состояния сети в App.kt

--- app/src/main/java/com/example/popularlibrariesapp/App.kt
+++ app/src/main/java/com/example/popularlibrariesapp/App.kt
@@ -1,6 +1,9 @@
 package com.example.popularlibrariesapp
 
 import android.app.Application
+import com.example.popularlibrariesapp.data.internet.AndroidNetworkStatus
+import com.example.popularlibrariesapp.data.room.Database
+import com.example.popularlibrariesapp.domain.internet.INetworkStatus
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 
@@ -8,14 +11,21 @@ class App : Application() {
     companion object {
         lateinit var instance: App
     }
+
     //TODO : Переместить
     private val cicerone: Cicerone<Router> by lazy {
         Cicerone.create()
     }
     val navigatorHolder get() = cicerone.getNavigatorHolder()
     val router get() = cicerone.router
+
+    lateinit var database: Database
+    lateinit var networkStatus: INetworkStatus
     override fun onCreate() {
         super.onCreate()
         instance = this
+        networkStatus = AndroidNetworkStatus(this)
+        Database.create(this)
+        database = Database.getInstance()
     }
 }
