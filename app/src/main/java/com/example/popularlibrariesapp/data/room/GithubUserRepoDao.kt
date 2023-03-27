3d0a7bcaa04d35ad5de4eb9c731d53112edd631e
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-27 18:14:23 +0300

    Добавили DAO для репозиториев и пользователей.

---
+++ app/src/main/java/com/example/popularlibrariesapp/data/room/GithubUserRepoDao.kt
@@ -0,0 +1,40 @@
+package com.example.popularlibrariesapp.data.room
+
+import androidx.room.*
+import com.example.popularlibrariesapp.domain.dto.room.RoomGithubUserReposEntity
+
+@Dao
+interface GithubUserRepoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: RoomGithubUserReposEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg users: RoomGithubUserReposEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(users: List<RoomGithubUserReposEntity>)
+
+    @Update
+    fun update(user: RoomGithubUserReposEntity)
+
+    @Update
+    fun update(vararg users: RoomGithubUserReposEntity)
+
+    @Update
+    fun update(users: List<RoomGithubUserReposEntity>)
+
+    @Delete
+    fun delete(user: RoomGithubUserReposEntity)
+
+    @Delete
+    fun delete(vararg users: RoomGithubUserReposEntity)
+
+    @Delete
+    fun delete(users: List<RoomGithubUserReposEntity>)
+
+    @Query("SELECT * FROM RoomGithubUserReposEntity")
+    fun getAll(): List<RoomGithubUserReposEntity>
+
+    @Query("SELECT * FROM RoomGithubUserReposEntity WHERE userId = :userId")
+    fun findForUser(userId: String): List<RoomGithubUserReposEntity>
+}
